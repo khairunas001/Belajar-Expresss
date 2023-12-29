7c285c8be4b0abd18d9a9ed00c3f25eb487434be
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2023-12-29 21:01:27 +0700

    manual querry dengan sequelize

--- controller/TodoController.js
+++ controller/TodoController.js
@@ -1,16 +1,13 @@
+const {todo} = require('../models')
 class TodoController {
     static getTodos(req,res){
-        const arrObjects = [
-            {
-                id:1,
-                name:"laptop"
-            },
-            {
-                id:2,
-                name:"personal computer"
-            }
-        ]
-        res.json(arrObjects);
+        todo.findAll()
+            .then(todos =>{
+                res.json(todos)
+            })
+            .catch(err =>{
+                res.json(err)
+            })
     }
 
     static addTodos (req,res){
